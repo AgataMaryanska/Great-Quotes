b637d00eb2b7748b8d82395a4673a461b2e1dca8
Agata <agata.maryanska@gmail.com>
2022-02-03 21:19:43 +0100

    adding-quotes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,31 @@
+import {useParams, Route} from 'react-router-dom';
+import {Fragment} from "react";
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+    {id: 'q1', author: 'Max', text: 'Learning React Is Fun'},
+    {id: 'q2', author: 'Maximilian', text: 'Learning React Is Great'},
+];
+
 const QuoteDetail = () => {
-    return <h1>Quote Detail Page</h1>
+    const params = useParams();
+
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+    if (!quote) {
+        return <p>No quote found!</p>;
+    }
+
+    return (
+        <Fragment>
+            <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments/>
+            </Route>
+        </Fragment>
+    );
 };
 
 export default QuoteDetail;
